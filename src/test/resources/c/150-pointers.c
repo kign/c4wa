f384c88dd4471ce85cc579dc31b5c1b1ebb21209
Konstantin Ignatiev <kostya@inet-lab.net>
2021-11-08 23:08:59 -0500

    Enchanced 150-pointers test

--- src/test/resources/c/150-pointers.c
+++ src/test/resources/c/150-pointers.c
@@ -1,12 +1,14 @@
 void printf();
 
 extern int main () {
-    int *a = alloc(10, 2, int);
+    int *a = alloc(10, 3, int);
     a[0] = 15;
-    *(a + 1) = 13;
-    a[2] = a[0] * a[1];
-    printf("%d * %d = %d\n", a[0], a[1], *(a + 2));
+    int* b = a + 1;
+    *b = 13;
+    *(a + 2) = 11;
+    a[3] = *a * *b * a[2];
+    printf("%d * %d * %d = %d\n", a[0], a[1], a[2], *(a + 3));
 
     return 0;
 }
-// 15 * 13 = 195
+// 15 * 13 * 11 = 2145
