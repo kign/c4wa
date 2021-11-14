41f5d9169142b8fdc9c29f6b3317f6d2e64165ed
Konstantin Ignatiev <kostya@inet-lab.net>
2021-11-14 16:18:23 -0500

    Added 108-smoke test

---
+++ src/test/resources/c/108-smoke.c
@@ -0,0 +1,18 @@
+void printf();
+
+int foo(int ret) {
+    printf("called foo(%d)\n", ret);
+    return ret;
+}
+
+extern int main () {
+    printf("Trying to evaluate foo(10)? foo(20): foo(30)\n");
+    int res = foo(10)? foo(20): foo(30);
+    printf("Result is %d\n", res);
+
+    return 0;
+}
+// Trying to evaluate foo(10)? foo(20): foo(30)
+// called foo(10)
+// called foo(20)
+// Result is 20
